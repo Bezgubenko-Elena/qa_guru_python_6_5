d8f28d2e20f74dd3cdecbe676211c1d5f6ed1cfa
Bezgubenko Helen <protasova-ea@yandex.ru>
2023-06-18 21:27:08 +0300

    fill out forms

---
+++ test.py
@@ -0,0 +1,41 @@
+import pytest
+from selene import browser
+from selene import be, have, by
+
+browser.config.window_width = 1920
+browser.config.window_height = 1080
+
+browser.open('https://demoqa.com/automation-practice-form')
+browser.element('[id="firstName"]').type('Helen')
+browser.element('[id="lastName"]').type('Bezgubenko')
+browser.element('[id="userEmail"]').type('eb@e.ru')
+browser.all('#genterWrapper .custom-control').element_by(have.exact_text('Female')).click()
+browser.element('[id="userNumber"]').type('9011111111')
+browser.element('[id="dateOfBirthInput"]').click()
+browser.element('.react-datepicker__year-select').click()
+browser.element('[value="1993"]').click()
+browser.element('.react-datepicker__month-select').click()
+browser.element('[value="10"]').click()
+browser.element('[class="react-datepicker__day react-datepicker__day--001"]').click()
+browser.element('[id="subjectsInput"]').type('a')
+browser.element('[id="react-select-2-option-3"]').click()
+browser.all('#hobbiesWrapper .custom-control').element_by(have.exact_text('Music')).click()
+browser.element('[id="uploadPicture"]').send_keys('E:\github\qa_guru_python_6_5\h.jpg')
+browser.element('[id="currentAddress"]').type('NN')
+browser.element('[id="state"]').click()
+browser.element('[id="react-select-3-option-0"]').click()
+browser.element('[id="city"]').click()
+browser.element('[id="react-select-4-option-0"]').click()
+browser.element('[id="submit"]').click()
+
+browser.element('[class="modal-title h4"]').should(have.text('Thanks for submitting the form'))
+browser.element('.table-responsive .table tbody tr')[0].should(have.text('Helen Bezgubenko'))
+browser.all('.table-responsive .table tbody tr')[1].should(have.text('eb@e.ru'))
+browser.all('.table-responsive .table tbody tr')[2].should(have.text('Female'))
+browser.all('.table-responsive .table tbody tr')[3].should(have.text('9011111111'))
+browser.all('.table-responsive .table tbody tr')[4].should(have.text('01 November,1993'))
+browser.all('.table-responsive .table tbody tr')[5].should(have.text('Social Studies'))
+browser.all('.table-responsive .table tbody tr')[6].should(have.text('Music'))
+browser.all('.table-responsive .table tbody tr')[7].should(have.text('h.jpg'))
+browser.all('.table-responsive .table tbody tr')[8].should(have.text('NN'))
+browser.all('.table-responsive .table tbody tr')[9].should(have.text('NCR Delhi'))
